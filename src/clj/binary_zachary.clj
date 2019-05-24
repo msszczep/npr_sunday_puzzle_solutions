a6b2ff314803be72ceaab7e7ba779049fc584a3b
Mitchell Szczepanczyk <mitchell@szcz.org>
2019-05-23 22:37:12 -0500

    Fixing bug with binary_zachary.clj

--- src/clj/binary_zachary.clj
+++ src/clj/binary_zachary.clj
@@ -4,17 +4,19 @@
   (str (reduce str (repeat (- n (count s)) "0")) s))
 
 (def list-of-numbers
-  (let [max-number 101
+  (let [max-number 128
         max-number-bin-count (count (Integer/toBinaryString max-number))]
    (map (juxt identity #(pad-zero (Integer/toBinaryString %)
                                   max-number-bin-count)) 
         (range max-number))))
 
 (defn filter-by-digit [d n]
-  (= \1 (nth n d)))
+  (= \1 (nth (reverse n) (/ (Math/log d) (Math/log 2)))))
 
 (defn get-answer [slot]
   (->> list-of-numbers
       (filter (comp (partial filter-by-digit slot) second))
       (map first)))
      
+
+; (get-answer 4)
