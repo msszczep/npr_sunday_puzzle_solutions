dbf0b81734c40c6b13eac8625e09f99c30dedf40
Mitchell Szczepanczyk <mitchell@szcz.org>
2019-07-21 10:02:45 -0500

    Updating puzzle statement.

--- src/clj/puzzle_2019-07-14.clj
+++ src/clj/puzzle_2019-07-14.clj
@@ -1,4 +1,7 @@
+; https://www.npr.org/2019/07/14/741543390/sunday-puzzle-lucky-number-7
 
+; Take an 11-letter word with two D's in it. If you drop both D's, you'll 
+; get a world capital followed by a sign of the zodiac. What's the 11-letter word?
 
 (defn has-two-ds? [word]
    (= 2 (get (frequencies word) \d)))
